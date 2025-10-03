640a841841ee0c03bd6b8efc6392df7bbcf779d2
Alexander Ronan <339828832@GoTVDSB.ca>
2025-10-03 08:50:35 -0400

    Add project files.

---
+++ Rock Paper Scissors/Program.cs
@@ -0,0 +1,53 @@
+﻿namespace Rock_Paper_Scissors
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            bool done = false;
+            while (!done)
+            {
+                Rock();
+                Thread.Sleep(1000);
+                Console.Clear();
+                Paper();
+                Thread.Sleep(1000);
+                Console.Clear();
+                Scissors();
+                Thread.Sleep(1000);
+                Console.Clear();
+            }
+        }
+
+        public static void Rock()
+        {
+            Console.WriteLine("    _______");
+            Console.WriteLine("---'   ____)");
+            Console.WriteLine("      (_____)");
+            Console.WriteLine("      (_____)");
+            Console.WriteLine("      (____)");
+            Console.WriteLine("---.__(___)");
+        }
+
+        public static void Paper()
+        {
+            Console.WriteLine("    _______");
+            Console.WriteLine("---'   ____)______");
+            Console.WriteLine("          ________)");
+            Console.WriteLine("          _________)");
+            Console.WriteLine("         _________)");
+            Console.WriteLine("---.____________)");
+
+        }
+
+        public static void Scissors()
+        {
+            Console.WriteLine("    _______");
+            Console.WriteLine("---'   ____)_____");
+            Console.WriteLine("          _______)");
+            Console.WriteLine("       ___________)");
+            Console.WriteLine("      (____)");
+            Console.WriteLine("---.__(___)");
+        }
+    }
+}
